79812268a7b292151a12a279d3b20ce62ae4bc80
CSC <634725355@qq.com>
2020-09-28 20:01:04 +0800

    轮播图

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,48 @@
 <template>
   <div id="app">
     <router-view />
+
+    <van-tabbar v-model="active">
+      <van-tabbar-item
+        @click="goToPage('home')"
+        :icon="active ? home : activeHome"
+        >首页</van-tabbar-item
+      >
+      <van-tabbar-item
+        @click="goToPage('user')"
+        :icon="active ? activeUser : user"
+        >我的</van-tabbar-item
+      >
+    </van-tabbar>
   </div>
 </template>
 
-<style>
+<script>
+import activeHome from "./assets/active-home.png";
+import home from "./assets/home.png";
+import activeUser from "./assets/active-user.png";
+import user from "./assets/user.png";
+
+export default {
+  data() {
+    return {
+      active: 0,
+      activeHome,
+      home,
+      activeUser,
+      user,
+    };
+  },
+  created() {},
+  methods: {
+    goToPage(name) {
+      this.$router.push({ name });
+    },
+  },
+};
+</script>
+
+<style lang="less" scoped>
+#app {
+}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,35 @@
 <template>
-  <div class="home"></div>
+  <div class="home">
+    <van-swipe class="home-swipe" :autoplay="3000">
+      <van-swipe-item v-for="(item, index) in images" :key="index">
+        <img :src="item" alt="" />
+      </van-swipe-item>
+    </van-swipe>
+  </div>
 </template>
 
 <script>
+import banner1 from "../assets/banner1.png";
+import banner3 from "../assets/banner3.png";
+
 export default {
   name: "Home",
-  components: {},
+  data() {
+    return {
+      images: [banner1, banner3],
+    };
+  },
+  methods: {},
 };
 </script>
+
+<style lang="less" scoped>
+.home-swipe .van-swipe-item {
+  color: #fff;
+  font-size: 20px;
+  line-height: 150px;
+  text-align: center;
+  background-color: #39a9ed;
+}
+</style>
+
