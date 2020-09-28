e9c2b4b94e9399a90ffefbebe44bfdd80222adfb
CSC <634725355@qq.com>
2020-09-28 19:21:43 +0800

    项目初始化

---
+++ vue.config.js
@@ -0,0 +1,58 @@
+const webpack = require("webpack");
+// 导入compression-webpack-plugin
+const CompressionWebpackPlugin = require('compression-webpack-plugin')
+// 定义压缩文件类型
+const productionGzipExtensions = ['js', 'css']
+module.exports = {
+    /* 部署生产环境和开发环境下的URL：可对当前环境进行区分，baseUrl 从 Vue CLI 3.3 起已弃用，要使用publicPath */
+    /* baseUrl: process.env.NODE_ENV === 'production' ? './' : '/' */
+    publicPath: './',
+    /* 输出文件目录：在npm run build时，生成文件的目录名称 */
+    outputDir: 'dist',
+    /* 放置生成的静态资源 (js、css、img、fonts) 的 (相对于 outputDir 的) 目录 */
+    assetsDir: "assets",
+    /* 是否在构建生产包时生成 sourceMap 文件，false将提高构建速度 */
+    productionSourceMap: false,
+    /* 默认情况下，生成的静态资源在它们的文件名中包含了 hash 以便更好的控制缓存，你可以通过将这个选项设为 false 来关闭文件名哈希。(false的时候就是让原来的文件名不改变) */
+    filenameHashing: false,
+    /* 代码保存时进行eslint检测 */
+    lintOnSave: true,
+    /* webpack-dev-server 相关配置 */
+    devServer: {
+        /* 自动打开浏览器 */
+        open: false,
+        /* 设置为0.0.0.0则所有的地址均能访问 */
+        host: '0.0.0.0',
+        port: 3789,
+        https: false,
+        hotOnly: false,
+        disableHostCheck: true,
+        /* 使用代理 */
+
+    },
+    // 不让压缩占位符
+    chainWebpack(config) {
+        // set whitespace
+        config.module
+            .rule("vue")
+            .use("vue-loader")
+            .loader("vue-loader")
+            .tap(options => {
+                options.compilerOptions.whitespace = 'preserve';
+                return options;
+            })
+            .end();
+    },
+    configureWebpack: {
+        plugins: [
+            new CompressionWebpackPlugin({
+                filename: '[path].gz[query]',
+                algorithm: 'gzip',
+                test: new RegExp('\\.(' + productionGzipExtensions.join('|') + ')$'),
+                threshold: 10240,
+                minRatio: 0.8
+            })
+        ]
+    }
+
+}
